472d274ef018adcd43f638a69adab02ba382b17f
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-11 21:18:16 +0300

    try_except trainings

--- lessons/return.py
+++ lessons/return.py
@@ -1,8 +1,19 @@
-def mathem(x, y):
-    sum_1 = x + y
-    return sum_1
-
-mathem(12, 17)
-print(mathem(14, 19))
+import math
 
 
+def circutArea(radius):
+    return math.pi * radius ** 2
+
+
+while True:
+    try:
+        r = int(input('Введите радиус: '))
+        # if r > 0:
+        break
+        # else:
+        #     print('Введите число больше 0')
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+
+area = circutArea(r)
+print(f'Площадь круга = {area:.2f}')
