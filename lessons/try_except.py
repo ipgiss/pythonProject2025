b041fea1636426a3e464af7bf8dfe31e975a0b1d
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-14 13:51:51 +0300

    2.28 try_except

--- lessons/try_except.py
+++ lessons/try_except.py
@@ -1,19 +1,37 @@
-import math
+# import math
+#
+#
+# def circutArea(radius):
+#     return math.pi * radius ** 2
+#
+#
+# while True:
+#     try:
+#         r = int(input('Введите радиус: '))
+#         # if r > 0:
+#         break
+#         # else:
+#         #     print('Введите число больше 0')
+#     except Exception as e:
+#         print(f'Произошла ошибка {e}')
+#
+# area = circutArea(r)
+# print(f'Площадь круга = {area:.2f}')
 
+try:
+    a = int(input())
+    b = int(input())
+    result = a / b
+except(ZeroDivisionError, ValueError) as er:
+    print(er)
+else:
+    print(f'Результат деления = {result}')
 
-def circutArea(radius):
-    return math.pi * radius ** 2
-
-
-while True:
-    try:
-        r = int(input('Введите радиус: '))
-        # if r > 0:
-        break
-        # else:
-        #     print('Введите число больше 0')
-    except Exception as e:
-        print(f'Произошла ошибка {e}')
-
-area = circutArea(r)
-print(f'Площадь круга = {area:.2f}')
+# try:
+#     a = int(input())
+#     b = int(input())
+#     c = a / b
+# except ZeroDivisionError:
+#     print('На ноль делить нельзя!')
+# except ValueError:
+#     print('Нужно вводить только числа!')
