17a4977fba58c000bc990aff2f8f9962ff782c5e
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-16 16:35:20 +0300

    first Class ))

---
+++ lessons/Classes.py
@@ -0,0 +1,10 @@
+class Alphabet:
+
+    def __init__(self, title, abbr, list):
+        self.title = title
+        self.abbr = abbr
+        self.list = list
+        print('Ok')
+
+
+russian = Alphabet('Russian', 'RU', 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
