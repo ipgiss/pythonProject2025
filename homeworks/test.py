f2099ae4282b79ffe7b051014c1bbd7cc2b4075c
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-20 16:53:35 +0300

    add 2 folder to test fetch_rebase

--- homeworks/test.py
+++ homeworks/test.py
@@ -1,3 +1,16 @@
-check for add new commit with new email
+def user_input(numb):
+    while True:
+        try:
+            return input(numb)
+        except ValueError:
+            print('only numbers pls')
 
+numb1 = user_input('Enter the 1st number : ')
+print(numb1)
 
+def input_number(prompt):          # 1. Объявляем функцию
+    while True:                   # 2. Запускаем вечный цикл
+        try:
+            return float(input(prompt))  # 3. Возвращаем число (если получилось)
+        except ValueError:               # 4. Если ошибка — цикл повторяется
+            print("Ошибка: введите число!")
