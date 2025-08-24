e6cc568f984f70ce5568488cd7a72673baa97301
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-24 13:43:39 +0300

    inheritance 1

--- Classes/person_2.py
+++ Classes/person_2.py
@@ -1,12 +1,37 @@
 class Person2:
-    def __init__(self, name, age, weight):
+    def __init__(self, name, age, height):
         self.name = name
         self.age = age
-        self.weight = weight
+        self.height = height
+        self.weight = 100  # если заводим постоянное значение, то из init убираем weight.
 
     def calories(self):
-        return self.weight /2
+        print(f'Для вас, {self.name}, оптимальный вес = {self.height / 100} кг')
 
-man = Person2('Vasya', 25, 100)
+    def description(self):
+        print(f'Человека зовут {self.name}, возраст {self.age} лет, рост {self.height} см и вес {self.weight} кг')
 
-print(man.calories())
+    def get_weight(self):
+        print(f'Вес человека = {self.weight} кг.')
+
+    def update_weight(self, kg):
+        self.weight = kg
+
+
+man = Person2('Vasya', 25, 170)
+man2 = Person2('Olya', 31, 160)
+
+# man.calories()
+man.description()
+man2.description()
+man.update_weight(90)
+man.get_weight()
+
+
+'''Изучаю Наследование'''
+class Warrior(Person2):
+    '''Создаем класс Warrior как Наследник класса Person'''
+
+    def __init__(self, name, age, height):
+        super().__init__(name, age, height)
+
