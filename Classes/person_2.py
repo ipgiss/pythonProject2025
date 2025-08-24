e34b9431b15c84443bb87bfedef909e456348697
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-24 14:10:32 +0300

    inheritance 2 (warrior example)

--- Classes/person_2.py
+++ Classes/person_2.py
@@ -22,16 +22,34 @@ man = Person2('Vasya', 25, 170)
 man2 = Person2('Olya', 31, 160)
 
 # man.calories()
-man.description()
-man2.description()
-man.update_weight(90)
-man.get_weight()
+# man.description()
+# man2.description()
+# man.update_weight(90)
+# man.get_weight()
 
 
 '''Изучаю Наследование'''
+
+
 class Warrior(Person2):
     '''Создаем класс Warrior как Наследник класса Person'''
 
     def __init__(self, name, age, height):
         super().__init__(name, age, height)
+        self.rage = 100
 
+    def get_rage(self):
+        '''Получениея значения "ярость" для героя'''
+        print(f'Заряд ярости героя = {self.rage}')
+
+    def description(self):
+        '''Переопределяем метод Родителя'''
+        description = f'Человека зовут {self.name}, возраст {self.age} лет, его ярость {self.rage}'
+        # print(description)
+        return description
+
+warrior_1 = Warrior('Canon', 30, 200)
+
+warrior_1.update_weight(150)
+print(warrior_1.description()) # вызов функции если в ней return
+# warrior_1.get_rage()
